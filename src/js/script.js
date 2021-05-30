a5d0958b8799bde119efcc0c10014a90320fad61
Vadym <vadymkorol1@gmail.com>
2021-05-31 01:42:52 +0300

    food

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+require('es6-promise').polyfill();
+var merge = require('package-merge');
+var dst = fs.readFileSync('package_gulp.json');
+var src = fs.readFileSync('package.json');
+import 'nodelist-foreach-polyfill';
+
+import calc from './modules/calc';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import modal from './modules/modal';
+import slider from './modules/slider';
+import tabs from './modules/tabs';
+import timer from './modules/timer';
+import openModal from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+
+    calc();
+    cards();
+    forms('form', modalTimerId);
+    modal('[data-modal]', '.modal', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    timer('.timer', '2022-06-11');
+
+});
